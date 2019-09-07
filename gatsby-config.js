80add861271544ea024545e27f86b9705671b61e
kengsheng <kengsheng@live.com>
2019-09-07 16:00:54 +0800

    Initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,30 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
+const urljoin = require("url-join")
+const config = require("./data/SiteConfig")
 
 module.exports = {
-  /* Your site config here */
+  pathPrefix: config.pathPrefix === "" ? "/" : config.pathPrefix,
+  siteMetadata: {
+    siteUrl: urljoin(config.siteUrl, config.pathPrefix),
+    rssMetadata: {
+      site_url: urljoin(config.siteUrl, config.pathPrefix),
+      feed_url: urljoin(config.siteUrl, config.pathPrefix, config.siteRss),
+      title: config.siteTitle,
+      description: config.siteDescription,
+      image_url: `${urljoin(
+        config.siteUrl,
+        config.pathPrefix
+      )}/logos/logo-512.png`,
+      copyright: config.copyright,
+    },
+  },
+  plugins: [
+    "gatsby-plugin-sass",
+    "gatsby-plugin-react-helmet",
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: config.googleAnalyticsID,
+      },
+    },
+  ],
 }
